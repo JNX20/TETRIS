248686db239be002537f707f07dfaa036154652e
JNX20 <167472197+JNX20@users.noreply.github.com>
2024-06-04 23:14:28 +0200

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,57 +1,109 @@
-#include "Joc.h"
-#include "Tauler.h"
-#include "Figura.h"
-#include <string>
+#if defined(WIN32) || defined(WIN32) || defined(WIN32) || defined(NT_) || defined  (_WIN64)
+
 #include <iostream>
+//Definicio necesaria per poder incloure la llibreria i que trobi el main
+#define SDL_MAIN_HANDLED
+#include <windows.h>
+//Llibreria grafica
+#include "../Graphic Lib/libreria.h"
+#include "../Graphic Lib/NFont/NFont.h"
+#include <conio.h>      /* getch */
 
-using namespace std;
+#elif _APPLE_
+//Llibreria grafica
+#include "../Graphic Lib/libreria.h"
+#include "../Graphic Lib/NFont/NFont.h"
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
+#include <SDL2/SDL.h>
+#pragma clang diagnostic pop
 
-/*void printFigura(Joc& prova1)
+#endif
+
+#include "./Partida.h"
+#include "./InfoJoc.h"
+
+#include "GraphicManager.h"
+#include "Tetris.h"
+
+int main(int argc, const char* argv[])
 {
-	cout << "\n" << "\n" << "\n";
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 4; j++)
-		{
-			cout << prova1.getFigura(i, j) << " ";
-		}
-		cout << "\n";
-	}
-	cout << "\n" << "\n" << "\n";
-}*/
+    SDL_SetMainReady();
+    SDL_Init(SDL_INIT_VIDEO);
+    Screen pantalla(SCREEN_SIZE_X, SCREEN_SIZE_Y);
 
-void printTauler(Joc& prova1)
-{
-	cout << "\n" << "\n" << "\n";
-	for (int i = 0; i < 8; i++)
-	{
-		for (int j = 0; j < 8; j++)
-		{
-			cout << prova1.getTauler(i, j) << " ";
-		}
-		cout << "\n";
-	}
-	cout << "\n" << "\n" << "\n";
-}
+    Tetris tetris;
+    bool final = false;
+    int instruccio = 0;
 
-int main()
-{
-	Joc prova1;
+    string nomFitxerPuntuacions;
+    cout << "Introdueix el nom del fitxer on es guardaran les puntuacions: ";
+    cin >> nomFitxerPuntuacions;
 
-	prova1.inicialitza("lectura.txt");
-	prova1.print();
-	printTauler(prova1);
-	prova1.baixaFigura();
-	prova1.print();
-	
-	
 
-	//prova1.giraFigura(GIR_HORARI);
-	//prova1.print();
+    ifstream fitxerPuntuacions;
+    fitxerPuntuacions.open(nomFitxerPuntuacions);
+    if (fitxerPuntuacions.is_open())
+    {
+        string nomTemporal;
+        int puntuacioTemporal;
 
-	//printFigura(prova1);
-	//printFigura(prova1);
-	//prova1.escriuTauler("Resultat.txt");
-	
-	//cout << "Fila: " << prova1.getFila() << "  Columna: " << prova1.getColumna() << "\n" << "\n" << "\n";
+        while (!fitxerPuntuacions.eof())
+        {
+            fitxerPuntuacions >> nomTemporal >> puntuacioTemporal;
+            if (!fitxerPuntuacions.eof())
+            {
+                tetris.afegeixPuntuacio(nomTemporal, puntuacioTemporal);
+            }
+                
+        }
+    }
+    fitxerPuntuacions.close();
+
+    do
+    {
+        cout << "\n" << "MENU PRINCIPAL" << "\n";
+        cout << "============" << "\n";
+        cout << "1. Joc en mode normal" << "\n";
+        cout << "2. Joc en mode test" << "\n";
+        cout << "3. Mostrar puntuacions" << "\n";
+        cout << "4. Sortir" << "\n";
+
+        string nomFitxerTauler, nomFitxerFigures, nomFitxerMoviments;
+        cin >> instruccio;
+        switch (instruccio)
+        {
+        case 1:
+            tetris.juga(pantalla, nomFitxerTauler, nomFitxerFigures, nomFitxerMoviments);
+            break;
+
+        case 2:
+            cout << "Nom del fitxer amb l'estat inicial del tauler: ";
+            cin >> nomFitxerTauler;
+            cout << "Nom del fitxer amb la sequencia de figures: ";
+            cin >> nomFitxerFigures;
+            cout << "Nom del fitxer amb la sequencia de moviments: ";
+            cin >> nomFitxerMoviments;
+            tetris.jugaModeTests(pantalla, nomFitxerTauler, nomFitxerFigures, nomFitxerMoviments);
+            break;
+
+        case 3:
+            tetris.mostraPuntuacions();
+            break;
+
+        case 4:
+            final = true;
+            break;
+
+        default:
+            cout << "ERROR" << "\n";
+            break;
+        }
+
+    } while (!final);
+
+    SDL_Quit();
+
+    tetris.passarPuntuacioFitxer(nomFitxerPuntuacions);
+    return 0;
 }
