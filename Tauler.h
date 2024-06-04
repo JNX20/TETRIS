04613c092dfe0246c58d1e25e2e11a8092e9328c
JNX20 <167472197+JNX20@users.noreply.github.com>
2024-06-04 23:20:43 +0200

    Tauler.h

--- Tauler.h
+++ Tauler.h
@@ -2,8 +2,8 @@
 #define TAULER_H
 #include "Figura.h"
 
-const int MAX_FILA = 8;
-const int MAX_COL = 8;
+const int MAX_FILA = 21;
+const int MAX_COL = 11;
 
 class Tauler
 {
@@ -11,6 +11,7 @@ public:
     Tauler();
 
     int getTauler(int fila, int columna) { return m_tauler[fila][columna]; }
+    int getImatge(int fila, int columna) { return (m_tauler[fila][columna] + 1); }
     void setTauler(int fila, int columna, int valor) { m_tauler[fila][columna] = valor; }
     int comprovarFiles();
     void eliminaFila(int fila);
